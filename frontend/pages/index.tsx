88b026d5aa22b3d2e3accf331626e4a8c95dd148
django_freeman <o.tampodo.t.18@gmail.com>
2023-01-24 12:07:00 +0900

    windowオブジェクトを型アサーション

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,26 +2,21 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 import { Button } from "@mui/material";
 
-interface Window {
-  ethereum: any;  // 苦肉の策
-}
-
 interface WalletProps {
   currentAccount: string;
   // setterAccount: (account: string) => void;
   setCurrentAccount: (value: React.SetStateAction<string>) => void
 }
 
-function connectWallet( ) { 
+function connectWallet() { 
   const requestEthAccounts = async () => {
-    let window : Window
-    const { ethereum } = window.ethereum;
-    if (!ethereum) throw new Error("error");
-    const accouts: string = await ethereum.request({ method: "eth_requestAccounts"});
-    if (accouts.length === 0) throw new Error("error");
+    if ((window as any).ethereum) console.log("ethereum not found")
+    const accouts: string = await (window as any).ethereum.request({ method: "eth_requestAccounts"});
+    if (accouts.length === 0) console.log("account not found")
     const accout = accouts[0];
     console.log(accout);
   }
+  requestEthAccounts()
 }
 
 const IndexPage = () => (
